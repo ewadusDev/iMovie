bdb289ea76fc5f9d7452f55e8447208410f30499
RUK KEOSUNG <rukzeedza@gmail.com>
2025-04-21 19:35:40 +0700

    feat: add client connection of mongodb and miniodb

---
+++ lib/db.ts
@@ -0,0 +1,55 @@
+"use server"
+import mongoose from "mongoose";
+import * as Minio from 'minio'
+
+const MONGO_URI = process.env.MONGO_URI
+const MINIO_ACCESSKEY = process.env.MINIO_ACCESSKEY
+const MINIO_SECRETKEY = process.env.MINIO_SECRETKEY
+const MINIO_ENDPOINT = process.env.MINIO_ENDPOINT
+const MINIO_PORT = process.env.MINIO_PORT
+// const MINIO_BUCKET = process.env.MINIO_BUCKET
+
+
+export const connectMongoDB = async () => {
+
+    if (mongoose.connection && mongoose.connection.readyState >= 1) {
+        console.log("Already connected to existed mongoDB");
+        return
+    }
+
+    try {
+        await mongoose.connect(MONGO_URI, {
+            dbName: "mydatabase",
+        })
+        console.log("Connected to MongoDB")
+    } catch (err) {
+        console.error("Error connecting to MongoDB", err)
+    }
+}
+
+
+
+export const minioClient = async () => {
+    const client = new Minio.Client({
+        endPoint: MINIO_ENDPOINT,
+        port: MINIO_PORT,
+        useSSL: false,
+        accessKey: MINIO_ACCESSKEY,
+        secretKey: MINIO_SECRETKEY,
+    })
+
+    // console.log("trying to connect to minio database")
+
+
+    try {
+        const resposne = await client.listBuckets()
+        console.log(resposne)
+        console.log("minio database connected")
+
+    } catch (err) {
+        console.log(err)
+    }
+
+
+
+}
