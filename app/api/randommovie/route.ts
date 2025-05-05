2355d1e5d2e9a87b6918b30e739e0644a04686b1
RUK KEOSUNG <rukzeedza@gmail.com>
2025-05-05 10:28:31 +0700

    feat: create random movie api

---
+++ app/api/randommovie/route.ts
@@ -0,0 +1,19 @@
+import prismadb from "@/lib/prismadb";
+import { NextResponse } from "next/server";
+
+
+export async function GET() {
+    try {
+        const movieCount = await prismadb.movie.count()
+        const randomIndex = Math.floor(Math.random() * movieCount)
+
+        const randomMovies = await prismadb.movie.findMany({
+            take: 1,
+            skip: randomIndex
+        })
+        return NextResponse.json(randomMovies[0], { status: 200 })
+
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 })
+    }
+}
