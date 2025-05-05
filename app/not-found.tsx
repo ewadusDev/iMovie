d131e76589b11f26743d1d5222255d384a74fb94
RUK KEOSUNG <rukzeedza@gmail.com>
2025-05-05 10:28:50 +0700

    feat: create not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,6 @@
+const NotFoundPage = () => {
+    return <div className="text-4xl text-white font-bold text-center h-screen flex items-center justify-center">
+        <p>I will implement this page later, sorry for inconvenience ＿|￣|○</p>
+    </div>;
+};
+export default NotFoundPage
